5c6f681777974eb71fb231c0f719a59b7cd65c22
matchaeggtart <matchaeggtart@hotmail.com>
2022-07-10 04:07:19 +0800

    update

---
+++ 03_nvim_lua/05_cmp_autopairs.sh
@@ -0,0 +1,6 @@
+cd ~/Downloads 
+git clone https://github.com/windwp/nvim-autopairs
+cd ~/Downloads/nvim-autopairs/lua/nvim-autopairs/completion
+cp cmp.lua ~/.local/share/nvim/site/pack/packer/start/nvim-autopairs/lua/nvim-autopairs/completion
+cp handlers.lua ~/.local/share/nvim/site/pack/packer/start/nvim-autopairs/lua/nvim-autopairs/completion
+cd ~
