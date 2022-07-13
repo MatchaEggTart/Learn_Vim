a4747677ae64ca2509e8a819ef470c00da80946c
matchaeggtart <matchaeggtart@hotmail.com>
2022-07-13 15:42:10 +0800

    update

---
+++ 05_learn_nvim_lua/01_install_dependence.sh
@@ -0,0 +1,14 @@
+# nvim 安装
+sudo dnf install vim vim-X11 xsel -y
+sudo dnf install neovim -y
+
+# 插件管理
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# telescope 快速搜索依赖
+sudo dnf install ripgrep -y
+sudo dnf install fd-find -y
+
+# nvim-treesitter 语法高亮
+sudo dnf install gcc-c++ -y
+sudo dnf install nodejs -y
