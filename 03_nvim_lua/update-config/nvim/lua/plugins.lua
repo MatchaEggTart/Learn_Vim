3c0ce0e7f85d5c4527faef990f0f920b5d08242b
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-09 20:29:01 +0800

    update

--- 03_nvim_lua/update-config/nvim/lua/plugins.lua
+++ 03_nvim_lua/update-config/nvim/lua/plugins.lua
@@ -11,9 +11,9 @@ packer.startup({
     --------------------- tools ---------------------------
     -- nvim-tree
     use ({
-      'kyazdani42/nvim-tree.lua',
+      'nvim-tree/nvim-tree.lua',
       requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        'nvim-tree/nvim-web-devicons', -- optional, for file icons
       },
       tag = 'nightly' -- optional, updated every week. (see issue #1193)
     })
@@ -46,10 +46,12 @@ packer.startup({
 
     -- 自定义源
     git = {
+      -- default_url_format = "https://hub.fastgit.org/%s",
       -- default_url_format = "https://hub.fastgit.xyz/%s",
       -- default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
       -- default_url_format = "https://gitcode.net/mirrors/%s",
-      -- default_url_format = "https://gitclone.com/github.com/%s",
+      default_url_format = "https://gitclone.com/github.com/%s",
+      -- default_url_format = "https://github.com.cnpmjs.org/%s",
     },
 
     -- 以浮动窗口打开安装列表
